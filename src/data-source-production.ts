94767449c4965d42fbf3687e3041149269dd9b72
stopcallingmyname <x666rxse@gmail.com>
2025-03-05 22:09:12 +0100

    Fix: Database Without Migrations

--- src/data-source-production.ts
+++ src/data-source-production.ts
@@ -4,15 +4,16 @@ import { User } from './entities/user.entity';
 import { Project } from './entities/project.entity';
 import { Tag } from './entities/tag';
 import { Subscription } from './entities/subscription.entity';
+import { EnvVariable } from './enum/env-variable.enum';
 
 const dataSource = new DataSource({
-  type: 'postgres',
-  host: 'dpg-crjueol2ng1s73fl3s9g-a',
-  database: 'nullable_dev_tdct',
-  port: 5432,
-  username: 'stopcallingmyname',
-  password: 'hc3Zb4JT0kmafezhT61eA147xBIe7A5t',
-  synchronize: false,
+  type: process.env[EnvVariable.DataBaseType] as 'postgres',
+  host: process.env[EnvVariable.DataBaseHost],
+  port: Number(process.env[EnvVariable.DataBasePort]),
+  username: process.env[EnvVariable.DataBaseUserName],
+  password: process.env[EnvVariable.DataBasePassword],
+  database: process.env[EnvVariable.DataBaseName],
+  synchronize: process.env[EnvVariable.IsDataBaseSynchronize] === 'true',
   entities: [User, Profile, Project, Tag, Subscription],
   migrations: ['src/migrations/*{.ts,.js}'],
 });
--- src/data-source.ts
+++ src/data-source.ts
@@ -4,15 +4,16 @@ import { User } from './entities/user.entity';
 import { Project } from './entities/project.entity';
 import { Tag } from './entities/tag';
 import { Subscription } from './entities/subscription.entity';
+import { EnvVariable } from './enum/env-variable.enum';
 
 const dataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  database: 'nullabledb_dev',
-  port: 5432,
-  username: 'postgres',
-  password: 'x666rxse',
-  synchronize: false,
+  type: process.env[EnvVariable.DataBaseType] as 'postgres',
+  host: process.env[EnvVariable.DataBaseHost],
+  port: Number(process.env[EnvVariable.DataBasePort]),
+  username: process.env[EnvVariable.DataBaseUserName],
+  password: process.env[EnvVariable.DataBasePassword],
+  database: process.env[EnvVariable.DataBaseName],
+  synchronize: process.env[EnvVariable.IsDataBaseSynchronize] === 'true',
   entities: [User, Profile, Project, Tag, Subscription],
   migrations: ['src/migrations/*{.ts,.js}'],
 });
